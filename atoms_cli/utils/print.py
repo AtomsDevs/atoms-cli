cccf7ec5140487c79e600df8298e876c36f68770
mirkobrombin <send@mirko.pm>
2022-08-15 18:17:09 +0200

    use tabulate for tables

--- atoms_cli/utils/print.py
+++ atoms_cli/utils/print.py
@@ -1,4 +1,5 @@
 import sys
+from tabulate import tabulate
 
 
 class Print:
@@ -26,25 +27,7 @@ class Print:
     def table(columns: list, rows: list, exit: bool=False):
         if len(columns) != len(rows[0]):
             Print.stderr("Invalid table. Columns and rows must have the same length.")
-
-        max_lengths = [max(len(row[i]) for row in rows) for i in range(len(columns))]
-
-        Print.stdout("|", new_line=False)
-        for i in range(len(columns)):
-            Print.stdout(f" {columns[i]} ", new_line=False)
-            Print.stdout(" " * (max_lengths[i] - len(columns[i])), new_line=False)
-            Print.stdout("|", new_line=False)
-
-        Print.stdout("\n")
-
-        for row in rows:
-            Print.stdout("|", new_line=False)
-            for i in range(len(row)):
-                Print.stdout( f" {row[i]} ", new_line=False)
-                Print.stdout(" " * (max_lengths[i] - len(row[i])), new_line=False)
-                Print.stdout("|", new_line=False)
-                
-            Print.stdout("\n")
-
-
-        Print.exit(exit)
+            
+        Print.stdout(tabulate(rows, headers=columns))
+        if exit:
+            Print.exit()
