21dfbac855383b644bb66d023b38e5f5e88066fd
mirkobrombin <send@mirko.pm>
2022-08-15 19:02:43 +0200

    implement list interface

--- atoms_cli/interfaces/list.py
+++ atoms_cli/interfaces/list.py
@@ -14,6 +14,41 @@ class ListAtom:
     def __validate_args(self):
         if self.__args.type not in atom_types:
             Print.stderr("Invalid atom type. Supported types: {}.".format(str_atom_types))
+        
+    def __get_atom_type(self, atom: 'Atom'):
+        return "container" if atom.is_distrobox_container else "chroot"
 
     def run(self):
-        Print.stdout("interface: ListAtom", exit=True)
+        rows = []
+        columns = ["Name", "Distribution", "Created", "Updated"]
+
+        if self.__args.type in ["container", "all"]:
+            columns += ["Container ID", "Container Image"]
+        
+        if self.__args.type == "all":
+            columns += ["Type"]
+        
+        for atom in self.__atoms_backend.atoms.values():
+            _row = [
+                atom.name, 
+                atom.distribution.name,
+                atom.creation_date, 
+                atom.update_date
+            ]
+
+            if self.__args.type in ["container", "all"]:
+                if atom.is_distrobox_container:
+                    _row += [
+                        atom.container_id, 
+                        atom.container_image
+                    ]
+                else:
+                    _row += ["n/a", "n/a"]
+
+            if self.__args.type == "all":
+                _row += [self.__get_atom_type(atom)]
+
+            rows.append(_row)
+
+        Print.table(columns, rows, exit=True)
+
